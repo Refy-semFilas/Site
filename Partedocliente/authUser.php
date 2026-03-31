9ed8f927c9fe04e21e555557efeea39e14468815
Aluno.Etec <aula.etec.com>
2026-03-31 16:20:20 -0300

    arrumando bebidas e filtro, Imagem do nome dos usuarios

--- Partedocliente/authUser.php
+++ Partedocliente/authUser.php
@@ -1,10 +1,12 @@
 <?php
 require "../supabaseConnection.php";
 
-$user = $_POST['user'];
+$user = trim($_POST['user']);
 $senha = $_POST['senha'];
+$result = supabaseRequest("/rest/v1/usuarios?username=eq." . urlencode($user) . "&select=*");
 
-$result = supabaseRequest("/rest/v1/usuarios?username=eq.$user&select=*");
+// Debug - remove in production
+file_put_contents("debug.txt", print_r($result, true));
 
 if (count($result['data']) === 1) {
     $dados = $result['data'][0];
